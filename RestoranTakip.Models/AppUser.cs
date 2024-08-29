0b9c6f2409787a497ed58dd7adfd24f9cef30040
Ömer Kutlay <omer.kutlay@outlook.com>
2024-08-30 02:29:22 +0300

    AppUser Modeli oluşturuldu

---
+++ RestoranTakip.Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Models
+{
+    public class AppUser:BaseModel
+    {
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+        public ICollection<Table> Tables { get; set; }
+    }
+}
