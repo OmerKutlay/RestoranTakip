9d19ea30834c50355557dbea80b89e653a9abe78
Ömer Kutlay <omer.kutlay@outlook.com>
2024-08-30 20:32:37 +0300

    Repository katmanı oluşturuldu

---
+++ RestoranTakip.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> predicate);
+        T GetById(int id);
+        T Add(T entity);
+        void Delete(int id);
+        List<T> AddRange(List<T> entities);
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate);
+        void Save();
+        T Update(T entity);
+    }
+}
---
+++ RestoranTakip.Repository/Shared/Concrete/Repository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestoranTakip.Data;
+using RestoranTakip.Models;
+using RestoranTakip.Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Repository.Shared.Concrete
+{
+    public class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public  T Add(T entity)
+        {
+            _dbSet.Add(entity);
+            Save();
+            return entity;
+        }
+
+        public List<T> AddRange(List<T> entities)
+        {
+            _dbSet.AddRange(entities);
+            Save();
+            return entities;
+        }
+
+        public void Delete(int id)
+        {
+            T entity = _dbSet.Find(id);
+            entity.IsDeleted = true;
+
+            Update(entity);
+        }
+
+        public  IQueryable<T> GetAll()
+        {
+            return  _dbSet.Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+           return _dbSet.Where(predicate);
+        }
+
+        public T GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.FirstOrDefault(predicate);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public T Update(T entity)
+        {
+            _dbSet.Update(entity);
+            Save();
+            return entity;
+        }
+    }
+}
+       
