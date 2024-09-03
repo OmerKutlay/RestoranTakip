bf759c3a8071f64424b60b90a7141aee2ad9a63e
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-03 15:47:21 +0300

    IOrderService interface'i oluşturuldu

---
+++ RestoranTakip.Business/Abstract/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using RestoranTakip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Abstract
+{
+    public interface IOrderService
+    {
+        Order Add(Order order, List<OrderDetail> orderDetails);
+        Order Update(Order order);
+        bool Delete(Order orderId);
+        ICollection<Order> GetAll(int tableId);
+        Order GetById(int orderId);
+    }
+}
