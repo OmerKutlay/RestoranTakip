b30c5f5c281278c8a6a41f99df80ff24d9746476
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-03 15:05:01 +0300

    IUserSevice business interface'i oluşturuldu

---
+++ RestoranTakip.Business/Abstract/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.PowerBI.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Abstract
+{
+    public interface IUserService
+    {
+        AppUser Add(AppUser appuser);
+        AppUser Update(AppUser appuser);
+        bool Delete(AppUser appuser);
+        AppUser CheckUser(string username, string password);
+    }
+}
