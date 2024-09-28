402e216ebd74f7abb33ed5b6a45ea4304f56bf85
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-28 19:52:34 +0300

    IStatusService interface'i oluşturuldu

---
+++ RestoranTakip.Business/Abstract/IStatusService.cs
@@ -0,0 +1,19 @@
+﻿using RestoranTakip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Abstract
+{
+    public interface IStatusService
+    {
+        IQueryable<Status> GetAll();
+        IQueryable<Status> GetAll(Order order);
+        Status GetById(int id);
+        Status Add(Status status);
+        Status Update(Status status);
+        bool Delete(int id);
+    }
+}
