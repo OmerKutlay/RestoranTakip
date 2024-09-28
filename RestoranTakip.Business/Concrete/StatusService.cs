0ad020544b5c176dc8af7bca416d6d39cf53ad36
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-28 20:15:00 +0300

    StatusService metotları yazıldı

---
+++ RestoranTakip.Business/Concrete/StatusService.cs
@@ -0,0 +1,46 @@
+﻿using RestoranTakip.Business.Abstract;
+using RestoranTakip.Models;
+using RestoranTakip.Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Concrete
+{
+    public class StatusService : IStatusService
+    {
+        private readonly IRepository<Status> _statusRepository;
+
+        public StatusService(IRepository<Status> statusRepository)
+        {
+            _statusRepository = statusRepository;
+        }
+        public IQueryable<Status> GetAll()
+        {
+            return _statusRepository.GetAll(s => !s.IsDeleted);
+        }
+        public IQueryable<Status> GetAll(Order order)
+        {
+            return _statusRepository.GetAll(s => s.Orders == order);
+        }
+        public Status GetById(int id)
+        {
+            return _statusRepository.GetById(id);
+        }
+        public Status Add(Status status)
+        {
+           return _statusRepository.Add(status);
+        }
+        public Status Update(Status status)
+        {
+           return _statusRepository.Update(status);
+        }
+        public bool Delete(int id)
+        {
+            _statusRepository.Delete(id);
+            return true;
+        }
+    }
+}
