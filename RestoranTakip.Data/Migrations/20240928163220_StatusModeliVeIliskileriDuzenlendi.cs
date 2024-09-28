be69d1643ddc68fc2b356619e42f32026f0be149
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-28 19:44:21 +0300

    Status modeli ve ilişkileri için Migration oluşturuldu

---
+++ RestoranTakip.Data/Migrations/20240928163220_StatusModeliVeIliskileriDuzenlendi.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestoranTakip.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StatusModeliVeIliskileriDuzenlendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalAmount",
+                table: "Orders",
+                newName: "TotalPrice");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StatusId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Status",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Status", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Status_StatusId",
+                table: "Orders",
+                column: "StatusId",
+                principalTable: "Status",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Status_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Status");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_StatusId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "TotalPrice",
+                table: "Orders",
+                newName: "TotalAmount");
+        }
+    }
+}
