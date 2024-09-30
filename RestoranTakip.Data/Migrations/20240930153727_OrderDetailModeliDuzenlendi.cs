2a50c177e84d8c1d47ac9bceeb36ef763bd4c9a0
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-30 18:45:57 +0300

    Database güncellendi

---
+++ RestoranTakip.Data/Migrations/20240930153727_OrderDetailModeliDuzenlendi.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestoranTakip.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderDetailModeliDuzenlendi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
