55b1f41282a25c1022418ba1cc3133b106d67227
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-30 18:28:43 +0300

    OrderDetail'ın Concrete class'ı oluşturuldu

---
+++ RestoranTakip.Business/Concrete/OrderDetailService.cs
@@ -0,0 +1,44 @@
+﻿using RestoranTakip.Business.Abstract;
+using RestoranTakip.Models;
+using RestoranTakip.Repository.Shared.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Concrete
+{
+    public class OrderDetailService : IOrderDetailService
+    {
+
+        private readonly IRepository<OrderDetail> _orderDetailRepository;
+
+        public OrderDetailService(IRepository<OrderDetail> orderDetailRepository)
+        {
+            _orderDetailRepository = orderDetailRepository;
+        }
+
+        public List<OrderDetail> GetAllByOrderId(int orderId)
+        {
+            return _orderDetailRepository.GetAll(od => od.OrderId == orderId).ToList();
+        }
+
+        public OrderDetail Add(OrderDetail orderDetail)
+        {
+            orderDetail.TotalAmount = orderDetail.ProductPrice * orderDetail.Quantity;
+            return _orderDetailRepository.Add(orderDetail);
+        }
+        public OrderDetail Update(OrderDetail orderDetail)
+        {
+            orderDetail.TotalAmount = orderDetail.ProductPrice * orderDetail.Quantity;
+            return _orderDetailRepository.Update(orderDetail);
+        }
+        public bool Delete(int id)
+        {
+            _orderDetailRepository.Delete(id);
+            return true;
+        }
+    }
+
+}
