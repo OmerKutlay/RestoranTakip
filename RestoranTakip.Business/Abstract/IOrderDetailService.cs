0f9eca995a8a8c1dbf47600f9bb4e03c0ff69e23
Ömer Kutlay <omer.kutlay@outlook.com>
2024-09-30 18:28:14 +0300

    OrderDetail için interface oluşturuldu

---
+++ RestoranTakip.Business/Abstract/IOrderDetailService.cs
@@ -0,0 +1,17 @@
+﻿using RestoranTakip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoranTakip.Business.Abstract
+{
+    public interface IOrderDetailService
+    {
+        List<OrderDetail> GetAllByOrderId(int orderId);
+        OrderDetail Add(OrderDetail orderDetail);
+        OrderDetail Update(OrderDetail orderDetail);
+        bool Delete(int id);
+    }
+}
